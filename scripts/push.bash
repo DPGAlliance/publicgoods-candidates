73f814a79f3a572e0a37534f24a728efe3f5b291
Victor Grau Serrat <lacabra@users.noreply.github.com>
2019-02-21 23:04:35 -0700

    BUG: fix relative paths

--- scripts/push.bash
+++ scripts/push.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-git clone https://github.com/unicef/publicgoods-wordpress.git
-git clone https://github.com/unicef/publicgoods-website.git
-git clone https://github.com/unicef/publicgoods-scripts.git
-pushd publicgoods-wordpress && ./develop.bash & && popd
-pushd publicgoods-scripts && ./static.bash && node index.js && popd
-git config --global user.email "lacabra@users.noreply.github.com "
+git clone https://github.com/unicef/publicgoods-wordpress.git ../publicgoods-wordpress
+git clone https://github.com/unicef/publicgoods-website.git ../publicgoods-website
+git clone https://github.com/unicef/publicgoods-scripts.git ../publicgoods-scripts
+pushd ../publicgoods-wordpress && (./develop.bash &) && popd
+pushd ../publicgoods-scripts && ./static.bash && npm install && node index.js && popd
+git config --global user.email "lacabra@users.noreply.github.com"
 git config --global user.name "Victor Grau Serrat"
-pushd publicgoods-website
+pushd ../publicgoods-website
 git remote set-url origin https://${GH_TOKEN}@github.com/unicef/publicgoods-website.git
-git commit -am "BLD: $TRAVIS_COMMIT" && git push --set-upstream origin/master master
+git commit -am "BLD: $TRAVIS_COMMIT" && git push --set-upstream origin master
