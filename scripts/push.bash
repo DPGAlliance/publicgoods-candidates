44eb10bc9659920a9401317afbd0b110b90b66fb
Victor Grau Serrat <lacabra@users.noreply.github.com>
2019-02-22 08:59:05 -0700

    MAINT: push not fail when nothing to commit

--- scripts/push.bash
+++ scripts/push.bash
@@ -6,4 +6,5 @@ git config --global user.email "lacabra@users.noreply.github.com"
 git config --global user.name "Victor Grau Serrat"
 pushd ../publicgoods-website
 git remote set-url origin https://${GH_TOKEN}@github.com/unicef/publicgoods-website.git
-git commit -am "BLD: $TRAVIS_COMMIT" && git push --set-upstream origin master
+git commit -am "BLD: $TRAVIS_COMMIT" || true
+git push --set-upstream origin master
