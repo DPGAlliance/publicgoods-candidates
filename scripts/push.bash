696b561f8b1ab622cb8be3e61833d297e21b667c
Victor Grau Serrat <lacabra@users.noreply.github.com>
2020-03-24 17:21:45 -0600

    BLD: remove travis CI

--- scripts/push.bash
+++ scripts/push.bash
@@ -6,5 +6,5 @@ git config --global user.email "lacabra@users.noreply.github.com"
 git config --global user.name "Victor Grau Serrat"
 pushd ../publicgoods-website
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/unicef/publicgoods-website.git
-git commit -am "BLD: $TRAVIS_COMMIT" || true
+git commit -am "BLD: $GITHUB_SHA" || true
 git push --set-upstream origin master
