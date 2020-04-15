72e896ea684fdb996a005531409db4111669285f
Victor Grau Serrat <lacabra@users.noreply.github.com>
2020-04-15 16:50:59 -0600

    BLD: push.bash succeeds only when everything succeeds

--- scripts/push.bash
+++ scripts/push.bash
@@ -1,10 +1,17 @@
 #!/bin/bash
-git clone https://github.com/unicef/publicgoods-website.git ../publicgoods-website
-git clone https://github.com/unicef/publicgoods-scripts.git ../publicgoods-scripts
-pushd ../publicgoods-scripts && ./static.bash && npm install && node index.js && node generate_nominees.js && npm run build && ./moveFiles.bash && popd
-git config --global user.email "lacabra@users.noreply.github.com"
-git config --global user.name "Victor Grau Serrat"
-pushd ../publicgoods-website
-git remote set-url origin https://${GITHUB_TOKEN}@github.com/unicef/publicgoods-website.git
-git commit -am "BLD: $GITHUB_SHA" || true
-git push --set-upstream origin master
+git clone https://github.com/unicef/publicgoods-website.git ../publicgoods-website && \
+	git clone https://github.com/unicef/publicgoods-scripts.git ../publicgoods-scripts && \
+    pushd ../publicgoods-scripts && \
+    	./static.bash && \
+    	npm install && \
+    	node index.js && \
+    	node generate_nominees.js && \
+    	npm run build && \
+    	./moveFiles.bash && \
+    popd && \
+	git config --global user.email "lacabra@users.noreply.github.com" && \
+	git config --global user.name "Victor Grau Serrat" && \
+	pushd ../publicgoods-website && \
+	git remote set-url origin https://${GITHUB_TOKEN}@github.com/unicef/publicgoods-website.git && \
+	git commit -am "BLD: $GITHUB_SHA" || true && \
+	git push --set-upstream origin master
